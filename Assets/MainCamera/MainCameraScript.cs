60837fef3b56936ba3c89afce28babd29cede42d
satoisamu2720 <115054959+satoisamu2720@users.noreply.github.com>
2025-04-25 15:31:42 +0900

    プレイヤー追従

--- Assets/MainCamera/MainCameraScript.cs
+++ Assets/MainCamera/MainCameraScript.cs
@@ -1,44 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class cameraManager : MonoBehaviour
+public class Camera : MonoBehaviour
 {
 
-    public GameObject target; // 追従する対象を決める変数
-    Vector3 pos;              // カメラの初期位置を記憶するための変数
+    public GameObject target;
 
-    // Start is called before the first frame update
+    [SerializeField]
+    private Vector3 cameraPos;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        pos = Camera.main.gameObject.transform.position; //カメラの初期位置を変数posに入れる
+        cameraPos = target.transform.position;
+        cameraPos.z = -10;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 cameraPos = target.transform.position; // cameraPosという変数を作り、追従する対象の位置を入れる
+        cameraPos.x = target.transform.position.x;
+        cameraPos.y = target.transform.position.y;
+        cameraPos.z = -15;
 
-        // もし対象の横位置が0より小さい場合
-        if (target.transform.position.x < 0)
-        {
-            cameraPos.x = 0; // カメラの横位置に0を入れる
-        }
+        this.transform.position = cameraPos;
 
-        // もし対象の縦位置が0より小さい場合
-        if (target.transform.position.y < 0)
-        {
-            cameraPos.y = 0;  // カメラの縦位置に0を入れる
-        }
-
-        // もし対象の縦位置が0より大きい場合
-        if (target.transform.position.y > 0)
-        {
-            cameraPos.y = target.transform.position.y;   // カメラの縦位置に対象の位置を入れる
-        }
-
-        cameraPos.z = -10; // カメラの奥行きの位置に-10を入れる
-        Camera.main.gameObject.transform.position = cameraPos; //　カメラの位置に変数cameraPosの位置を入れる
 
     }
+
+
+
 }
