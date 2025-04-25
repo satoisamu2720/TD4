484f85db96e447c07b1accbb6de1aa007524bcac
satoisamu2720 <115054959+satoisamu2720@users.noreply.github.com>
2025-04-25 16:57:51 +0900

    ガイド作成

---
+++ Assets/Guide/GuideScript.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class GuideScript:MonoBehaviour
+{
+    [SerializeField]
+    public GameObject target; // 対象のGameObjectへの参照
+    public float speed = 1f;            // 突っ込みスピード
+    public int maxHP = 2;               // 最大HP
+    private int currentHP;              // 現在のHP
+
+    private Transform destination;           // プレイヤーのTransform
+
+
+
+    void Start()
+    {
+        destination = GameObject.FindWithTag("Destination")?.transform;
+        currentHP = maxHP;
+    }
+
+    void Update()
+    {
+        RotateTowardsTarget();
+        if (destination != null)
+        {
+            transform.position = Vector3.MoveTowards(
+           transform.position,
+           target.transform.position,
+           speed * Time.deltaTime);
+
+            transform.position = new Vector2(
+            //移動範囲を制限する
+            Mathf.Clamp(transform.position.x, -8.0f, 8.0f),
+            Mathf.Clamp(transform.position.y, -4.5f, 4.5f)
+            );
+        }
+    }
+
+  
+
+    private void RotateTowardsTarget()
+    {
+        // ターゲットが設定されていなければ、処理をスキップ
+        if (target == null) return;
+
+        // ターゲットへの方向を算出
+        Vector3 targetDirection = CalculateDirectionTowardsTarget();
+
+        // ターゲットの方向へ回転させる
+        AlignRotationToTarget(targetDirection);
+    }
+
+    private Vector3 CalculateDirectionTowardsTarget()
+    {
+        return target.transform.position - transform.position;
+    }
+
+    private void AlignRotationToTarget(Vector3 direction)
+    {
+        transform.rotation = Quaternion.FromToRotation(Vector3.up, direction);
+    }
+}
